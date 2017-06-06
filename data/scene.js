2491aa8b5ef6142e41e9d1370a197b80407d5afd
K-Mertin <k.mertin@gmail.com>
2017-06-06 18:50:04 +0800

    Create scene.js

---
+++ data/scene.js
@@ -0,0 +1,10 @@
+var sceneModel = JSON.parse('{ "scenes" : [' +
+'{   "Id": "scene1",      "Name": "景福宮",      "EngName": "",      "KoreaName": "",' +
+    '"Address": "首爾特別市鐘路區社稷路161 景福宮",      "EngAddress": "",      "KoreaAddress": "서울특별시종로구사직로161 경복궁",' +
+    '"time": "09:00～18:30",      "trafficInfo": "地鐵3號線景福宮(Gyeongbokgung)站5號出口" },' +
+'{   "Id": "scene2",      "Name": "景福宮2",      "EngName": "",      "KoreaName": "",' +
+    '"Address": "首爾特別市鐘路區社稷路161 景福宮",      "EngAddress": "",      "KoreaAddress": "서울특별시종로구사직로161 경복궁",' +
+    '"time": "09:00～18:30",      "trafficInfo": "地鐵3號線景福宮(Gyeongbokgung)站5號出口" },' +
+    '{   "Id": "scene3",      "Name": "景福宮3",      "EngName": "",      "KoreaName": "",' +
+    '"Address": "首爾特別市鐘路區社稷路161 景福宮",      "EngAddress": "",      "KoreaAddress": "서울특별시종로구사직로161 경복궁",' +
+    '"time": "09:00～18:30",      "trafficInfo": "地鐵3號線景福宮(Gyeongbokgung)站5號出口" }  ]}');
