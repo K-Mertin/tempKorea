1af754b8e1d6c78beb58b93c3219147d75dc5121
K-Mertin <k.mertin@gmail.com>
2017-06-06 19:58:59 +0800

    Create schedule.js

---
+++ data/schedule.js
@@ -0,0 +1,9 @@
+var scheduleModel = JSON.parse('{ "schedule" : [' +
+'{   "Type": "Day1",      "STime": "6/17(六)",      "ETime": "",      "Location": "", "Price": ""},' +
+'{   "Type": "Day2",      "STime": "6/18(日)",      "ETime": "",      "Location": "", "Price": ""},' +
+'{   "Type": "Day3",      "STime": "6/19(一)",      "ETime": "",      "Location": "", "Price": ""},' +
+'{   "Type": "Day4",      "STime": "6/20(二)",      "ETime": "",      "Location": "", "Price": ""},' +
+'{   "Type": "Day5",      "STime": "6/21(三)",      "ETime": "",      "Location": "", "Price": ""},' +
+'{   "Type": "Day6",      "STime": "6/22(四)",      "ETime": "",      "Location": "", "Price": ""},' +
+'{   "Type": "Day7",      "STime": "6/23(五)",      "ETime": "",      "Location": "", "Price": ""}' +
+']}');
