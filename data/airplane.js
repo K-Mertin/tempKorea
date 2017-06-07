1127dab317f54ba5374d180ef537a156707f99d7
K-Mertin <k.mertin@gmail.com>
2017-06-07 17:36:02 +0800

    Create airplane.js

---
+++ data/airplane.js
@@ -0,0 +1,4 @@
+var airplaneModel = JSON.parse('{ "airplane" : [' +
+'{   "Id": "airplane1",      "Flight": "EVA Air BR160",      "Date": "2017/06/17",      "Departure": "TPE", "DepartureTime": "15:15",      "Arrive": "ICN",      "ArriveTime": "18:45" },' +
+'{   "Id": "airplane2",      "Flight": "EVA Air BR159",      "Date": "2017/06/23",      "Departure": "ICN", "DepartureTime": "19:45",      "Arrive": "TPE",      "ArriveTime": "21:20" }' +
+']}');
